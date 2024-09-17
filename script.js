995770e6fca5283d61e4cd13a1384d35d8517813
Andrtsit <Andrtsit@gmail.com>
2024-09-17 16:24:04 +0200

    Finished with the html/styling/added js file and sound on click

---
+++ script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/*
+                          HXOS STO KLIK          
+
+                          
+let audio = new Audio("click.mp3");
+let btn = document.querySelectorAll(".btn");
+let playFunction = function () {
+  audio.play();
+};
+
+for (let i = 0; i < btn.length; i++) {
+   btn[i].addEventListener("click", playFunction);
+ }
+
+btn.forEach((button) => {
+  button.addEventListener("click", playFunction);
+});
+
+
+*/
